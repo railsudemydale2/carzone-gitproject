14c7e6f0d5fb11f9fd38f12ac0b86369ad83c5cc
Dale Stewart <stewartdale073@gmail.com>
2021-06-03 10:49:31 -0400

    completed search functionalities and admin customization

--- cars/urls.py
+++ cars/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search'),
+
+
+
 ]
